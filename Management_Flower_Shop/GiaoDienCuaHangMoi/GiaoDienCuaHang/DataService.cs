48239c4b4931fa1a5dacf4d05dc197c18052f62c
hoangnam13102000 <hoangnam131020@gmail.com>
2022-12-08 22:52:35 +0700

    add

---
+++ Management_Flower_Shop/GiaoDienCuaHangMoi/GiaoDienCuaHang/DataService.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GiaoDienCuaHang
+{
+    public class DataService:DataTable
+    {
+        static SqlConnection m_Connection;
+        SqlDataAdapter m_DataAdapter;
+        SqlCommand m_Command;
+        string strConn = @"Data Source=DESKTOP-ON9V604\SQLEXPRESS01;Initial Catalog=QLCHmoi;Integrated Security=True";
+        public DataService()
+        {
+
+        }
+        public void Load(SqlCommand command)
+        {
+            if (m_Connection == null || m_Connection.State == ConnectionState.Closed)
+            {
+                m_Connection = new SqlConnection(strConn);
+                m_Connection.Open();
+            }
+            m_Command = command;
+            m_Command.Connection = m_Connection;
+
+            m_DataAdapter = new SqlDataAdapter(m_Command);
+            this.Clear();
+            m_DataAdapter.Fill(this);
+        }
+
+        public void Update()
+        {
+            SqlCommandBuilder builder = new SqlCommandBuilder(m_DataAdapter);
+            m_DataAdapter.Update(this);
+        }
+    }
+}
---
+++ Management_Flower_Shop/GiaoDienCuaHangMoi/GiaoDienCuaHang/frmThongTinPhanMem.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GiaoDienCuaHang
+{
+    public partial class FormTTPM : Form
+    {
+        public FormTTPM()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
